fbcbead2e572ada90a02042aebef1bd35fccd50b
raidan <raidancia0@gmail.com>
2024-04-12 22:44:19 +0200

    Initial Commit

---
+++ Itranslator/ItranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItranslatorApp.swift
+//  Itranslator
+//
+//  Created by Raidan on 12/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ItranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
